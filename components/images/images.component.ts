6fed9c3281ab71368e7ca634769ab5f40876243b
blowedminds <ruzgardeniz.08@hotmail.com>
2018-07-01 22:26:15 +0300

    new design layout created, javascript optimized for backend server

--- components/images/images.component.ts
+++ components/images/images.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material';
-import { NgForm } from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 import { ImageAddComponent } from '../../dialogs/image-add/image-add.component';
 
@@ -19,8 +18,7 @@ export class ImagesComponent implements OnInit {
 
   THUMB_IMAGE_URL: string;
 
-  get isPageReady()
-  {
+  get isPageReady() {
     return !!this.images;
   }
 
@@ -28,40 +26,39 @@ export class ImagesComponent implements OnInit {
     public dialog: MatDialog,
     private imageRequestService: ImageRequestService,
     private helpersService: HelpersService
-  )
-  {
+  ) {
     this.THUMB_IMAGE_URL = this.imageRequestService.makeUrl('image.thumb');
   }
 
   ngOnInit() {
 
-    this.imageRequestService.getImages().subscribe( response => this.images = response);
+    this.imageRequestService.getImages().subscribe(response => this.images = response);
   }
 
-  makeImageUrl(image_id: string)
-  {
-    return this.THUMB_IMAGE_URL + image_id +'?token=' + this.helpersService.getToken()
+  makeImageUrl(image_id: string) {
+    return this.THUMB_IMAGE_URL + image_id + '?token=' + this.helpersService.getToken();
   }
 
-  openImageUploader()
-  {
-    let dialogRef = this.dialog.open(ImageAddComponent, {
+  openImageUploader() {
+    const dialogRef = this.dialog.open(ImageAddComponent, {
       disableClose: true
-    })
+    });
 
-    let rq2 = dialogRef.afterClosed().subscribe( result => {
-      if(!result) return
+    let rq2 = dialogRef.afterClosed().subscribe(result => {
+      if (!result) {
+        return;
+      }
 
-      this.images = null
+      this.images = null;
 
       let rq3 = this.imageRequestService.getImages().subscribe(response => {
-        this.images = response
-        rq3.unsubscribe()
-        rq3 = null
-      })
+        this.images = response;
+        rq3.unsubscribe();
+        rq3 = null;
+      });
 
-      rq2.unsubscribe()
-      rq2 = null
-    })
+      rq2.unsubscribe();
+      rq2 = null;
+    });
   }
 }
--- services/image-request.service.ts
+++ services/image-request.service.ts
@@ -7,73 +7,69 @@ import { catchError } from 'rxjs/operators';
 import { MainRequestService, HelpersService, RoutingListService } from '../imports';
 
 @Injectable()
-export class ImageRequestService extends MainRequestService{
+export class ImageRequestService extends MainRequestService {
 
   constructor(
     http: HttpClient,
     helpersService: HelpersService,
     routingListService: RoutingListService
-  )
-  {
+  ) {
     super(http, helpersService, routingListService);
   }
 
-  postImage(data: any, file: any): Observable<any>
-  {
+  putImage(data: any, file: any): Observable<any> {
     const url = this.makeUrl('image.image', '?token=' + this.helpersService.getToken());
 
     const formData = new FormData();
 
     formData.append('file', file);
 
-    for(const prop in data) {
+    for (const prop in data) {
+      if (data.hasOwnProperty(prop)) {
 
-      formData.append(prop, data[prop]);
+        formData.append(prop, data[prop]);
+      }
     }
 
     const req = new HttpRequest('POST', url, formData, {
       reportProgress: true,
-      headers: new HttpHeaders({ 'enctype': 'multipart/form-data', 'X-Requested-With': 'XMLHttpRequest'})
+      headers: new HttpHeaders({ 'enctype': 'multipart/form-data', 'X-Requested-With': 'XMLHttpRequest' })
     });
 
     return this.http
-                  .request(req)
-                  .pipe(catchError(error => this.handleError(error)));
+      .request(req)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  getImages(): Observable<any>
-  {
+  getImages(): Observable<any> {
     const url = this.makeUrl('image.images');
 
     return this.http
-                    .get(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .get(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  getEdit(image: string): Observable<any>
-  {
+  getEdit(image: string): Observable<any> {
     const url = this.makeUrl('image.edit', image);
 
     return this.http
-                    .get(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .get(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  putEdit(data: any): Observable<any>
-  {
-    const url = this.makeUrl('image.edit', data.u_id);
+  postImage(u_id: string, data: any): Observable<any> {
+    const url = this.makeUrl('image.edit', u_id);
 
     return this.http
-                    .put(url, JSON.stringify(data), this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .post(url, JSON.stringify(data), this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  deleteImage(u_id: string): Observable<any>
-  {
+  deleteImage(u_id: string): Observable<any> {
     const url = this.makeUrl('image.image', u_id);
 
     return this.http
-                    .delete(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .delete(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 }
