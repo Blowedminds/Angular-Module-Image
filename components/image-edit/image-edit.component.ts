b67b26b7712c4b260e96ff53f3ad7b3f9dae1be7
blowedminds <ruzgardeniz.08@hotmail.com>
2019-02-07 21:43:17 +0300

    design improved

--- components/image-edit/image-edit.component.ts
+++ components/image-edit/image-edit.component.ts
@@ -64,30 +64,24 @@ export class ImageEditComponent implements OnInit {
       return;
     }
 
-    const element = <HTMLImageElement> document.getElementById(id);
+    const element = <HTMLImageElement>document.getElementById(id);
 
     this.cropper = new Cropper(element, {
       scalable: false,
       zoomable: false,
       ready: () => this.cropperInitialized = true,
       crop: (e: any) => {
-        this.cropperData.x = parseInt(e.detail.x);
-        this.cropperData.y = parseInt(e.detail.y);
-        this.cropperData.width = parseInt(e.detail.width);
-        this.cropperData.height = parseInt(e.detail.height);
-        this.cropperData.rotate = parseInt(e.detail.rotate);
+        for (const key in this.cropperData) {
+          if (this.cropperData.hasOwnProperty(key)) {
+            this.cropperData[key] = e.detail[key].toFixed(0);
+          }
+        }
       }
     });
   }
 
-  setData(f: NgForm) {
-    this.cropper.setData({
-      x: parseInt(f.value.x),
-      y: parseInt(f.value.y),
-      width: parseInt(f.value.width),
-      height: parseInt(f.value.height),
-      rotate: parseInt(f.value.rotate),
-    });
+  setData(data: any) {
+    this.cropper.setData(data);
   }
 
   postImage(f: NgForm) {
